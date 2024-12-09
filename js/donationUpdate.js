281594e800005f63b3252d9a759b9fc0d5b67255
jakir-md <mdjakir.cse.ju@gmail.com>
2024-12-09 15:29:07 +0600

    mobile version blogging section done

--- js/donationUpdate.js
+++ js/donationUpdate.js
@@ -1,7 +1,7 @@
 document.getElementById('noakhaliDonateBtn').addEventListener('click', (event) => {
     const currentTotalBal = parseFloat(getCurrentBalance('myTotalBalance'));
     const modal = document.getElementById('my_modal_1');
-    let donatedTaka = getDonatedBalance('noakhaliDonateTaka');
+    let donatedTaka = getDonatedBalance('noakhaliDonatedTaka');
     if(donatedTaka === 'invalidInfo'){
         return;
     }else if(donatedTaka>currentTotalBal || donatedTaka<0){
@@ -13,10 +13,55 @@ document.getElementById('noakhaliDonateBtn').addEventListener('click', (event) =
         const updatedTotal = parseFloat(getCurrentBalance('noakhaliDonation')) + donatedTaka;
         document.getElementById('noakhaliDonation').innerText = updatedTotal;
         modal.showModal();
-        document.getElementById('closeModalBtn').addEventListener('click', () => {
+        document.getElementById('closeModalBtn1').addEventListener('click', () => {
             modal.close();
         });
         updateHistory('nokhaliDonationText', donatedTaka);
+    }
+});
+
+
+document.getElementById('feniDonateBtn').addEventListener('click', (event) => {
+    const currentTotalBal = parseFloat(getCurrentBalance('myTotalBalance'));
+    const modal = document.getElementById('my_modal_2');
+    let donatedTaka = getDonatedBalance('feniDonatedTaka');
+    if(donatedTaka === 'invalidInfo'){
+        return;
+    }else if(donatedTaka>currentTotalBal || donatedTaka<0){
+        alert("Invalid Donation!");
+    }else {
+        donatedTaka = parseFloat(donatedTaka);
+        const remainedTaka = currentTotalBal - donatedTaka;
+        document.getElementById('myTotalBalance').innerText = remainedTaka;
+        const updatedTotal = parseFloat(getCurrentBalance('feniDonation')) + donatedTaka;
+        document.getElementById('feniDonation').innerText = updatedTotal;
+        modal.showModal();
+        document.getElementById('closeModalBtn2').addEventListener('click', () => {
+            modal.close();
+        });
+        updateHistory('feniDonationText', donatedTaka);
+    }
+});
+
+document.getElementById('quotaDonateBtn').addEventListener('click', (event) => {
+    const currentTotalBal = parseFloat(getCurrentBalance('myTotalBalance'));
+    const modal = document.getElementById('my_modal_3');
+    let donatedTaka = getDonatedBalance('quotaDonatedTaka');
+    if(donatedTaka === 'invalidInfo'){
+        return;
+    }else if(donatedTaka>currentTotalBal || donatedTaka<0){
+        alert("Invalid Donation!");
+    }else {
+        donatedTaka = parseFloat(donatedTaka);
+        const remainedTaka = currentTotalBal - donatedTaka;
+        document.getElementById('myTotalBalance').innerText = remainedTaka;
+        const updatedTotal = parseFloat(getCurrentBalance('quotaDonation')) + donatedTaka;
+        document.getElementById('quotaDonation').innerText = updatedTotal;
+        modal.showModal();
+        document.getElementById('closeModalBtn3').addEventListener('click', () => {
+            modal.close();
+        });
+        updateHistory('quotaDonationText', donatedTaka);
     }
 });
 
@@ -37,5 +82,9 @@ document.getElementById('donationBtn').addEventListener('click', () => {
 });
 
 document.getElementById('blogBtnUp').addEventListener('click', () => {
-    window.location.href = '/blog.html';
+    window.location.href = 'blog.html';
+});
+
+document.getElementById('blogBtn').addEventListener('click', () => {
+    window.location.href = 'blog.html';
 });
